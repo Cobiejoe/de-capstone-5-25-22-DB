47c8131072d7e68caea25a84b4f8c137b2585242
Joe <josephcobarrubias@gmail.com>
2022-11-11 10:30:59 -0800

    Security Changes

--- db_connector.py
+++ db_connector.py
@@ -8,7 +8,7 @@ def table_to_upload(file_path):
     return file_to_upload_df
 
 
-connection = create_engine('mysql+pymysql://admin:pepper12@capstone-05-25.cfivfc6wcng0.us-west-2.rds.amazonaws.com/joes_swim_shack',
+connection = create_engine('URI, USERNAME, PASSWORD',
                            echo=False)
 
 upload_df = table_to_upload('csv_folders/payments.csv')
